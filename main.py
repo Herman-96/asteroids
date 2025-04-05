b94a3eb083fb1ea00d0adc8afb7cc551ab4fe420
HermanBentsrud <herman444@live.se>
2025-04-05 13:49:34 +0200

    Finished the project

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
 
 # Now import pygame (after the warnings filter)
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
 
 def main():
@@ -20,12 +24,52 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    pygame.display.set_caption("Asteroids")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
     while True:
+
+        dt = clock.tick(60) / 1000    # Calculate dt
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
+
+        updatable.update(dt)    # Update player and all other updatable objects
+
+        for asteroid in asteroids:
+            if player.is_colliding_with(asteroid):
+                print("Game over!")
+                import sys
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.is_colliding_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")    # Clear the screen
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()   # Update the display with all changes
 
 if __name__ == "__main__":
     main()
